a17790f8b8668c4bbdee9a5d3d0ae97cc8917381
Shravanreddy101 <shravanreddytx@gmail.com>
2025-04-16 01:03:47 -0500

    Fix static and media URL config

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('',include('store.urls'))
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
