f047b2a6a783c1906292f3e9f2476c4a93c01cde
Shravanreddy101 <shravanreddytx@gmail.com>
2025-04-15 22:32:37 -0500

    Added product image field and updated database

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
